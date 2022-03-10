0c402b84c636b679ca4ef62565752aa2739fe819
Tathagata <tathagata1805@gmail.com>
2022-03-10 17:53:53 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Builder from "./components/Builder";
+
+function App() {
+  return (
+    <div className="w-full h-auto p-2">
+      <Builder />
+    </div>
+  );
+}
+
+export default App;
